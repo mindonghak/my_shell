4b5ee4ea80f5e2932ba80db905e025e0f8c3fbbb
donghak <donghakhappy.gmail>
2022-04-02 11:06:25 -0700

    pipe done, no grep

--- proj1/my_shell.c
+++ proj1/my_shell.c
@@ -2,6 +2,8 @@
 #include "csapp.h"
 #include <errno.h>
 #define MAXARGS   128
+#define READ_END 0
+#define WRITE_END 1
 
 
 
@@ -9,9 +11,6 @@
 void eval(char *cmdline);
 void parseline(char *buf, char **argv, char *argvlist[128][MAXARGS], int *num, int *bg);
 int builtin_command(char **argv); 
-
-void my_mkdir(char **argv);
-void my_cd(char **argv);
 
 
 
@@ -37,17 +36,21 @@ int main()
 /* eval - Evaluate a command line */
 void eval(char *cmdline) 
 {
-    char *argv[MAXARGS]; /* Argument list execve() */
+	char *argv[MAXARGS]; /* Argument list execve() */
 	char *argvlist[128][MAXARGS];
 
 	char buf[MAXLINE];   /* Holds modified command line */
-    int bg[128];              /* Should the job run in bg or fg? */
-    pid_t pid;           /* Process id */
+	int bg[128];              /* Should the job run in bg or fg? */
+	pid_t pid[128];           /* Process id */
 	int num=0;	//how many pipes
-    
+	int pipefd[2];	
+
+
 	strcpy(buf, cmdline);
 	parseline(buf, argv, argvlist, &num, bg); 
+//	argv = argvlist[0];
 	int i,j;
+/*
 	for(i=0;i<=num;i++){
 		j=0;
 		while(argvlist[i][j] !=NULL){
@@ -55,37 +58,97 @@ void eval(char *cmdline)
 		}
 		printf("next argv\n");
 	}
-
-
-	if (argv[0] == NULL)  
+*/
+//printf("here??\n");
+	if (argvlist[0][0] == NULL)  
 		return;   /* Ignore empty lines */
-/*
-    if (!builtin_command(argv)) { //quit -> exit(0), & -> ignore, other -> run
-        if((pid = Fork())==0){
-		if (execvp(argv[0], argv) < 0) {	//ex) /bin/ls ls -al &
-           	 printf("%s: Command not found.\n", argv[0]);
-           	 exit(0);
-        	}
-	}
+	if(num==0){	// No pipe
+//printf("here???\n");
+		if (!builtin_command(argvlist[0])) { //quit -> exit(0), & -> ignore, other -> run
+//printf("here?\n");
+			if((pid[0] = Fork())==0){
+				if (execvp(argvlist[0][0], argvlist[0]) < 0) {	//ex) /bin/ls ls -al &
+        	   		 printf("%s: Command not found.\n", argvlist[0][0]);
+        	   		 exit(0);
+        			}
+			}
 
-	/* Parent waits for foreground job to terminate *
-	if (!bg){
-		int status;
-		if(waitpid(pid, &status,0)<0){
-			unix_error("waitfg: waitpid error");
+		/* Parent waits for foreground job to terminate */
+			if (!bg[0]){
+				int status;
+				if(waitpid(pid[0], &status,0)<0){
+					unix_error("waitfg: waitpid error");
+				}
+			}
+			else//when there is backgrount process!
+			    printf("%d %s\n", pid[0], cmdline);
+    			}
+		else{
+			if(!strcmp(argvlist[0][0],"cd")){
+				if(chdir(argvlist[0][1]) < 0){
+					printf("cd error\n");
+				}
+			}
 		}
 	}
-	else//when there is backgrount process!
-	    printf("%d %s", pid, cmdline);
-    }
-	else{
-		if(!strcmp(argv[0],"cd")){
-			if(chdir(argv[1]) < 0){
+	else if(num>0){
+		for(i=0;i<num;i++){
+			if(pipe(pipefd) == -1){
+				fprintf(stderr, "parent: Failed to create pipe\n");
+				return;
+			}
+			if(i==0){
+				if(!builtin_command(argvlist[0])){
+					if((pid[i] = Fork())==0){
+						if(dup2(pipefd[WRITE_END],STDOUT_FILENO) == -1){
+							fprintf(stderr, "first child dup2 failed\n");
+						}
+						close(pipefd[READ_END]);
+						close(pipefd[WRITE_END]);
+						if (execvp(argvlist[0][0], argvlist[0]) < 0) {        //ex) /bin/ls ls -al &
+							printf("%s: Command not found.\n", argv[0]);
+							exit(0);
+						}
+					}
+				}
+			}
+//printf("here?\n");
+			if(!builtin_command(argvlist[i+1])){
+				if((pid[i+1] = Fork()) == 0){
+					if(dup2(pipefd[READ_END], STDIN_FILENO) == -1){
+						fprintf(stderr, "%d child dup2 failed\n",i+1);
+					}
+					close(pipefd[READ_END]);
+					close(pipefd[WRITE_END]);
+					if(execvp(argvlist[i+1][0], argvlist[i+1]) < 0){
+						printf("%d child %s: Command not found.\n", i+1, argvlist[i+1][0]);
+						exit(0);
+					}
+				}
+			}
+			close(pipefd[READ_END]);
+			close(pipefd[WRITE_END]);
+		}
+		for(i=0;i<=num;i++){
+			if (!bg[i]){
+                                int status;
+                                if(waitpid(pid[i], &status,0)<0){
+                                        unix_error("waitfg: waitpid error");
+                                }
+                        }
+                        else{//when there is backgrount process!
+                            printf("%d %s", pid[i], cmdline);
+                        }
+		}
+		if(!strcmp(argvlist[0][0],"cd")){
+			if(chdir(argvlist[0][1]) < 0){
 				printf("cd error\n");
 			}
 		}
 	}
-*/
+
+			
+
     return;
 }
 
@@ -135,10 +198,10 @@ void parseline(char *buf, char **argv, char *argvlist[128][MAXARGS], int *num, i
 	}
 	strncpy(temp,buf,itp);
 	temp[itp] = '\0';
-	printf("temp : %s\n",temp);
+//	printf("temp : %s\n",temp);
 	if(strchr(temp, '|')){
 		pipe = strchr(buf, '|');
-		printf("*buf = %c\n",*buf);
+//		printf("*buf = %c\n",*buf);
 		if(*buf != '|'){	// if not start with '|' 
 			argvlist[*num][argc++] = buf;	//store argv before '|'
 		}
